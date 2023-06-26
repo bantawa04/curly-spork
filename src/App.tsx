8de5d7c2e7f92c57ee431810fa375f5b8efd1b85
bantawa04 <rahunn3@gmail.com>
2023-06-26 12:31:25 +0545

    tailwind and eslint configured

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="columns-2">
+          <div>column 1</div>
+          <div>column 2</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
